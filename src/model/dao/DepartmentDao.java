be4506741d56b10ff12950f03f0f59f486e5bac1
João <joaokavalciuki@hotmail.com>
2024-04-29 12:16:27 -0300

    Função: DAO de departaqmento impplementada

---
+++ src/model/dao/DepartmentDao.java
@@ -0,0 +1,14 @@
+package model.dao;
+
+import model.entities.Department;
+
+import java.util.List;
+
+public interface DepartmentDao {
+
+    void insert(Department department);
+    void update(Department department);
+    void deleteById(Integer departmentId);
+    Department findById(Integer departmentId);
+    List<Department> findAll();
+}
